ec7f1db9c203c5e34c1d5ca922fb46b70470a338
vg1593 <66551337+vg1593@users.noreply.github.com>
2021-03-20 22:37:55 +0530

    Update views.py

--- mainapis/views.py
+++ mainapis/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from twilio.twiml.messaging_response import MessagingResponse
+from rest_framework.response import Response
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
 
 
@@ -9,4 +11,4 @@ class WhatsappApi(APIView):
         msg = request.POST.get('Body')
         resp = MessagingResponse()
         resp.message("You said: {}".format(msg))
-        return str(resp)
+        return HttpResponse(resp.to_xml, content_type='text/xml')
