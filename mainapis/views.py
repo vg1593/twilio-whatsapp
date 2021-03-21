98b7cdc5811fc40283f30a67e9e7fe804715dab5
root <root@ip-172-31-11-131.ap-south-1.compute.internal>
2021-03-21 18:37:27 +0000

    updated code

--- mainapis/views.py
+++ mainapis/views.py
@@ -3,19 +3,15 @@ from rest_framework.views import APIView
 from twilio.twiml.messaging_response import MessagingResponse
 from rest_framework.response import Response
 from django.http import JsonResponse, HttpResponse
-<<<<<<< HEAD
 from mainapis.models import Conversation
 from chatbot import generate_response
-=======
->>>>>>> 931fe76a7b980e43aa8b4c21ecabbbac26c3b8a6
 # Create your views here.
 
 
 class WhatsappApi(APIView):
     def post(self,request):
         msg = request.POST.get('Body')
-<<<<<<< HEAD
-        user = request.POST.get("from")[-10:]
+        user = request.POST.get("From")[-10:]
         conversation, created = Conversation.objects.get_or_create(sender=user)
         response,last,selection,language = generate_response(msg,conversation.last_selection,conversation.selections,conversation.language)
         conversation.message = response
@@ -26,8 +22,3 @@ class WhatsappApi(APIView):
         resp = MessagingResponse()
         resp.message(response)
         return HttpResponse(resp.to_xml(), content_type='text/xml')
-=======
-        resp = MessagingResponse()
-        resp.message("You said: {}".format(msg))
-        return HttpResponse(resp.to_xml(), content_type='text/xml')
->>>>>>> 931fe76a7b980e43aa8b4c21ecabbbac26c3b8a6
